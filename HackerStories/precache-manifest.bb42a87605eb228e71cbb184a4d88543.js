cb545cd507f386102ca50955a4f056cb4488343a
dstollbyu <elderstoll@gmail.com>
2020-07-23 10:35:53 -0600

    Try adding HackerStories again.

---
+++ HackerStories/precache-manifest.bb42a87605eb228e71cbb184a4d88543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb0c866bd7bd117729e7becc8a3e1ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e87ee0a27b3cbb7df9d3",
+    "url": "/static/css/main.2e784614.chunk.css"
+  },
+  {
+    "revision": "ed313217e472789e8f73",
+    "url": "/static/js/2.115aad5f.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.115aad5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87ee0a27b3cbb7df9d3",
+    "url": "/static/js/main.7cdefa83.chunk.js"
+  },
+  {
+    "revision": "b55014f044bba798a831",
+    "url": "/static/js/runtime-main.50faa5bf.js"
+  },
+  {
+    "revision": "46e24b694a11e0d3b351e5509722d167",
+    "url": "/static/media/check.46e24b69.svg"
+  }
+]);
